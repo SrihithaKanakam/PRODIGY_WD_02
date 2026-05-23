44d0c268e1a27c25167b3a37c5fb22370a26b617
SrihithaKanakam <srihitha750@gmail.com>
2026-05-23 18:19:14 +0530

    Add files via upload
    Inter active navigation menu created using HTML,CSS and javaScript.

---
+++ script.js
@@ -0,0 +1,13 @@
+window.addEventListener("scroll", function(){
+
+    let navbar =
+    document.getElementById("navbar");
+
+    if(window.scrollY > 50){
+        navbar.style.backgroundColor = "black";
+    }
+    else{
+        navbar.style.backgroundColor = "blue";
+    }
+
+});
